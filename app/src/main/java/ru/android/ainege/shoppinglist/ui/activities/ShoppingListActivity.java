8a5a11e5ebba69e5e4ead9872a26cacaeaa8da9c
Ainege <Ainege@gmail.com>
2015-07-31 14:10:05 +0300

    update ActionBarDrawerToggle to v7

--- app/src/main/java/ru/android/ainege/shoppinglist/ui/activities/ShoppingListActivity.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ui/activities/ShoppingListActivity.java
@@ -13,7 +13,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -63,11 +63,10 @@ public class ShoppingListActivity extends Activity implements LoaderManager.Load
         getActionBar().setHomeButtonEnabled(true);
 
         mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
+                this,
+                mDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
         ) {
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
