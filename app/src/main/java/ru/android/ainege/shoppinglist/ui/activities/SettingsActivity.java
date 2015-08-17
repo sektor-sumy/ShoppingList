dd4b462459e21165290ae9c015db946fdf25b392
Ainege <Ainege@gmail.com>
2015-08-17 16:04:48 +0300

    settings view

--- app/src/main/java/ru/android/ainege/shoppinglist/ui/activities/SettingsActivity.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ui/activities/SettingsActivity.java
@@ -1,17 +1,26 @@
 package ru.android.ainege.shoppinglist.ui.activities;
 
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import ru.android.ainege.shoppinglist.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 }
