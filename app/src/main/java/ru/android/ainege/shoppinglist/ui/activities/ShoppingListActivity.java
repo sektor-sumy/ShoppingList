ae3cc5df635d37ef0b08f2cdc5bee9605d27321e
Ainege <Ainege@gmail.com>
2015-10-06 14:30:43 +0300

    replace  action bar on toolbar

--- app/src/main/java/ru/android/ainege/shoppinglist/ui/activities/ShoppingListActivity.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ui/activities/ShoppingListActivity.java
@@ -16,6 +16,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -61,6 +62,9 @@ public class ShoppingListActivity extends AppCompatActivity implements LoaderMan
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         initDrawer();
 
