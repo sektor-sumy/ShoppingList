44983deabaeb8054dfd7d90fa3bf8d28d7cfc372
Ainege <Ainege@gmail.com>
2015-12-10 18:49:50 +0300

    update add button in dictionary settings

--- app/src/main/java/ru/android/ainege/shoppinglist/ui/fragments/settings/DictionaryFragment.java
+++ app/src/main/java/ru/android/ainege/shoppinglist/ui/fragments/settings/DictionaryFragment.java
@@ -67,7 +67,7 @@ public abstract class DictionaryFragment<T extends Dictionary> extends Fragment 
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_settings_data, container, false);
+		View v = inflater.inflate(R.layout.fragment_dictionary, container, false);
 
 		Button add = (Button) v.findViewById(R.id.add);
 		add.setOnClickListener(getAddHandler());
